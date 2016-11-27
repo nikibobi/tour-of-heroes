342c7a498d645aaa5a9770f575d8dcc14c9a12a2
bosak <bosakmaw@gmail.com>
2016-11-27 22:17:59 +0200

    added basic router

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,7 +9,16 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
   providers: [HeroService],
   bootstrap:    [ AppComponent ]
