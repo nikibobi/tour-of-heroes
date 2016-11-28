c183d6bc9a9ba223f965b8825e0deffdc07b7eab
bosak <bosakmaw@gmail.com>
2016-11-29 00:50:57 +0200

    deleting heroes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -36,6 +36,14 @@ export class HeroService {
             .catch(this.handleError);
     }
 
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
